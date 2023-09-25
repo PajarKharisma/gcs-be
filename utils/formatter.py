cbe2b674bee98a61316e294915985063413c8195
PajarKharisma <pajarkharisma@hotmail.com>
2023-09-25 11:25:00 +0700

    update format

--- utils/formatter.py
+++ utils/formatter.py
@@ -8,9 +8,9 @@ def formatData(rawData):
                 'roll': float(data[1].strip().split(" ")[0]),
                 'pitch': float(data[2].strip().split(" ")[0]),
                 'yaw': float(data[3].strip().split(" ")[0]),
-                'lat': float(data[4].strip().split(" ")[0]),
-                'long': float(data[5].strip().split(" ")[0]),
-                'alt': float(data[6].strip().split(" ")[0]),
+                'alt': float(data[4].strip().split(" ")[0]),
+                'lat': float(data[5].strip().split(" ")[0]),
+                'long': float(data[6].strip().split(" ")[0]),
             }
     except:
         pass
