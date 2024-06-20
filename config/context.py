172e877c96d74f4ee343f8070b1d91b0c292fb2c
PajarKharisma <pajarkharisma@hotmail.com>
2024-06-20 14:38:55 +0700

    update requirement.txt

--- config/context.py
+++ config/context.py
@@ -1,6 +1,21 @@
 global INPUT_VALUES
 global OUTPUT_VALUES
 
+VALUES = {
+    # OUTPUT VALUES
+    'pitch': 0,
+    'roll': 0,
+    'yaw': 0,
+    'lat': 0,
+    'long': 0,
+    'alt': 0,
+
+    # INPUT VALUES
+    'baudrate': 57600,
+    'port': 'COM3',
+    'app_connect': False
+}
+
 OUTPUT_VALUES = {
     'pitch': 0,
     'roll': 0,
